24ade0adcd6e7801ff847dedf8a082fdad94cc0a
marc7666 <marc7666@github.com>
2023-06-03 19:21:46 +0200

    Resolved nomenclature issues in tests

--- Tests/MixedEncodeTests.java
+++ Tests/MixedEncodeTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MixedEncodeTests {
     @Test
-    void upperCaseTest1() {
+    void mixedCaseTest1() {
         MaceroImpl test1 = new MaceroImpl("Marc", 3);
         StringBuilder expected = new StringBuilder("3.954 4.164 4.311 4.183 ");
         String actual = test1.encode();
@@ -12,7 +12,7 @@ public class MixedEncodeTests {
     }
 
     @Test
-    void upperCaseTest2() {
+    void mixedCaseTest2() {
         MaceroImpl test1 = new MaceroImpl("Cervera", 6);
         StringBuilder expected = new StringBuilder("2.347 2.576 2.643 2.663 2.576 2.643 2.553 ");
         String actual = test1.encode();
@@ -20,7 +20,7 @@ public class MixedEncodeTests {
     }
 
     @Test
-    void upperCaseTest3() {
+    void mixedCaseTest3() {
         MaceroImpl test1 = new MaceroImpl("Rosell", 11);
         StringBuilder expected = new StringBuilder("1.838 1.964 1.979 1.925 1.953 1.953 ");
         String actual = test1.encode();
